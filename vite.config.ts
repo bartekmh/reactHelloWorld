c0954b9f32364d4b07b012de777ee6abe64e9555
Bartosz <barteknaliwajko@wp.pl>
2025-09-20 21:35:50 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/reactHelloWorld/', // Replace <repo-name> with your GitHub repo name
   plugins: [react()],
 })
